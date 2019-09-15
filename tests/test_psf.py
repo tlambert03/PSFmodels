c84ca947ad329dba651d976b0d3e0c543283501e
Talley Lambert <talley.lambert@gmail.com>
2019-09-15 12:16:04 -0400

    add tests

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -6,11 +6,20 @@ import numpy as np
 class DeviceTest(unittest.TestCase):
     # This is a simple test that just tries to load the module
 
-    def test_make_psf(self):
+    def test_vectorial_psf(self):
         zvec = np.linspace(-1, 1, 5)
         p = psfm.vectorial_psf(zvec, nx=31)
         self.assertEqual(p.shape, (5, 31, 31))
 
-    def test_make_centered_psf(self):
+    def test_vectorial_psf_centered(self):
         p = psfm.vectorial_psf_centered(nx=31, nz=5)
+        self.assertEqual(p.shape, (5, 31, 31))
+
+    def test_scalar_psf(self):
+        zvec = np.linspace(-1, 1, 5)
+        p = psfm.scalar_psf(zvec, nx=31)
+        self.assertEqual(p.shape, (5, 31, 31))
+
+    def test_scalar_psf_centered(self):
+        p = psfm.scalar_psf_centered(nx=31, nz=5)
         self.assertEqual(p.shape, (5, 31, 31))
