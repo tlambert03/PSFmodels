740e86552053eb74e76ba3dfcbd32e1dfe08fd1c
Talley Lambert <talley.lambert@gmail.com>
2022-04-16 14:22:54 -0400

    update tests

--- tests/test_psf.py
+++ tests/test_psf.py
@@ -1,35 +1,31 @@
-import unittest
-
 import numpy as np
-
 import psfmodels as psfm
 
 
-class DeviceTest(unittest.TestCase):
-    # This is a simple test that just tries to load the module
+def test_vectorial_psf():
+    zvec = np.linspace(-1, 1, 5)
+    p = psfm.vectorial_psf(zvec, nx=31)
+    assert p.shape == (5, 31, 31)
 
-    def test_vectorial_psf(self):
-        zvec = np.linspace(-1, 1, 5)
-        p = psfm.vectorial_psf(zvec, nx=31)
-        self.assertEqual(p.shape, (5, 31, 31))
 
-    def test_vectorial_psf_deriv(self):
-        zvec = np.linspace(-1, 1, 5)
-        p, a, b, c = psfm.vectorial_psf_deriv(zvec, nx=31)
-        self.assertEqual(p.shape, (5, 31, 31))
-        self.assertEqual(a.shape, (5, 31, 31))
-        self.assertEqual(b.shape, (5, 31, 31))
-        self.assertEqual(c.shape, (5, 31, 31))
+def test_vectorial_psf_deriv():
+    zvec = np.linspace(-1, 1, 5)
+    results = psfm.vectorial_psf_deriv(zvec, nx=31)
+    assert len(results) == 4
+    assert all(x.shape == (5, 31, 31) for x in results)
 
-    def test_vectorial_psf_centered(self):
-        p = psfm.vectorial_psf_centered(nx=31, nz=5)
-        self.assertEqual(p.shape, (5, 31, 31))
 
-    def test_scalar_psf(self):
-        zvec = np.linspace(-1, 1, 5)
-        p = psfm.scalar_psf(zvec, nx=31)
-        self.assertEqual(p.shape, (5, 31, 31))
+def test_vectorial_psf_centered():
+    p = psfm.vectorial_psf_centered(nx=31, nz=5)
+    assert p.shape == (5, 31, 31)
 
-    def test_scalar_psf_centered(self):
-        p = psfm.scalar_psf_centered(nx=31, nz=5)
-        self.assertEqual(p.shape, (5, 31, 31))
+
+def test_scalar_psf():
+    zvec = np.linspace(-1, 1, 5)
+    p = psfm.scalar_psf(zvec, nx=31)
+    assert p.shape == (5, 31, 31)
+
+
+def test_scalar_psf_centered():
+    p = psfm.scalar_psf_centered(nx=31, nz=5)
+    assert p.shape == (5, 31, 31)
