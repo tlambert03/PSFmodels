7c29020325b4c2ce2842ac931d705035026653b1
Talley Lambert <talley.lambert@gmail.com>
2023-05-11 08:23:16 -0400

    update perf_plot

--- plot_perf.py
+++ plot_perf.py
@@ -1,15 +1,30 @@
 # benchmark the different implementations with perfplot
+import contextlib
+
 import numpy as np
 import perfplot
-from psfmodels.v2 import _li2017, jax
+from psfmodels.v2 import _li2017
+
+kernels = [lambda a: _li2017.radial_psf_li2017(a, a)]
+labels = ["numpy"]
+
+with contextlib.suppress(ImportError):
+    from psfmodels.v2 import jax
+
+    kernels.append(lambda a: jax.radial_psf_li2017(a, a))
+    labels.append("jax")
+
+with contextlib.suppress(ImportError):
+    from psfmodels.v2 import cupy
+
+    kernels.append(lambda a: cupy.radial_psf_li2017(a, a))
+    labels.append("cupy")
 
 perfplot.show(
     setup=lambda n: np.linspace(-3, 3, n),
-    kernels=[
-        lambda a: _li2017.radial_psf_li2017(a, a),
-        lambda a: jax.radial_psf_li2017(a, a),
-    ],
-    labels=["numpy", "jax"],
-    n_range=[2**k for k in range(4, 13)],
-    target_time_per_measurement=3,  # needs to be higher to allow jax to compile
+    kernels=kernels,
+    labels=labels,
+    n_range=[2**k for k in range(6, 14)],
+    xlabel="array size (n**2)",
+    target_time_per_measurement=4,  # needs to be higher to allow jax to compile
 )
